da8412e092746ad28fa9c11c4db3f217499f93ef
irfiglave <133179868+irfiglave@users.noreply.github.com>
2023-05-11 09:58:24 +0800

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,11 @@
+klf = input('Сгенирировать QRcode: ')
+klf_1 = input('Расшифровать QRcode: ')
+if klf ('Сгенирировать'):
+    print("Ок")
+else:
+    print("No ok")
+if klf_1 ('Расшифрвать'):
+    print('Ok')
+else:
+    print('No ok')
+    
